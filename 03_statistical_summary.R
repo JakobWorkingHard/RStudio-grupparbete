200986e4559993b17972a7b04037ae513a3a6666
salam8108 <salam.adel@hotmail.com>
2026-04-10 12:02:19 +0200

    Add statistical summary analysis

---
+++ 03_statistical_summary.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+
+data <- read_csv("ecommerce_orders.csv")
+
+data <- data %>%
+  mutate(order_value = quantity * unit_price)
+# Antal rader (antal ordrar)
+nrow(data)
+
+# Medelvärde av ordervärde
+mean(data$order_value, na.rm = TRUE)
+
+# Median av ordervärde
+median(data$order_value, na.rm = TRUE)
+# Jämförelse av ordervärde mellan kundsegment
+data %>%
+  group_by(customer_segment) %>%
+  summarise(
+    medel_order = mean(order_value, na.rm = TRUE),
+    median_order = median(order_value, na.rm = TRUE),
+    antal = n()
+  )
+# Returgrad per produktkategori
+data %>%
+  group_by(product_category) %>%
+  summarise(
+    returgrad = mean(return_status == "Yes", na.rm = TRUE),
+    antal = n()
+  )
+names(data)
+# Returgrad per produktkategori
+data %>%
+  group_by(product_category) %>%
+  summarise(
+    returgrad = mean(returned == "Yes", na.rm = TRUE),
+    antal = n()
+  )# Jämförelse av ordervärde beroende på rabatt
+data %>%
+  group_by(discount_pct) %>%
+  summarise(
+    medel_order = mean(order_value, na.rm = TRUE),
+    antal = n()
+  )
