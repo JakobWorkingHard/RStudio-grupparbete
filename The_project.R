7311287010fbe05dee965257254f8bdedc9ec216
Elza Capar <elza.capar@gmail.com>
2026-04-25 09:44:29 +0200

    Uppdatera huvudskript för reproducerbar körning

---
+++ The_project.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(janitor)
+library(scales)
+
+source("01_dataförståelse.R")
+source("02_datastädning.R")
+source("03_statistiska_summeringar.R")
+source("04_visualiseringar.R")
